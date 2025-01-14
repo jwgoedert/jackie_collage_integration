90a18e9325e29994f7b7cbed89be046d8b52891a
jwgoedert <jwgoedert@gmail.com>
2025-01-14 14:39:42 -0600

    remove console

--- renderProjects.js
+++ renderProjects.js
@@ -2,9 +2,7 @@ import { transformProjects } from './transformProjects.js';
 import { setVineContainerWidth, createSvgContainer, renderProjectNodes, renderVinePaths } from './svgUtils.js';
 
 export function renderProjects(projects, projectsByParent) {
-  console.log("Rendering vine...", projects);
   const vineData = transformProjects(projects);
-
   setVineContainerWidth(vineData);
   const svgElement = createSvgContainer();
   renderProjectNodes(svgElement, vineData);
