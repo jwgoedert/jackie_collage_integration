12ce54fe9ded573dc08ff48c95ecee505834f050
jwgoedert <jwgoedert@gmail.com>
2025-01-14 12:31:46 -0600

    move modal logic, remove unused variables

--- compLayout.js
+++ compLayout.js
@@ -1,15 +1,5 @@
 import { fetchProjectsByYear, fetchProjectsByParent } from './fetchProjects.js';
 import { renderProjects } from './renderProjects.js';
-import { openModal, normalizeName } from './modalUtils.js';
-
-// DOM Elements and Constants
-const vineContainer = document.getElementById("vine-container");
-const vineLine = document.getElementById("vine-line");
-const modal = document.getElementById("modal");
-const svgNamespace = "http://www.w3.org/2000/svg";
-const viewWidth = window.innerWidth;
-const viewHeight = window.innerHeight;
-const nodeSize = 300;
 
 // Initialize the Application
 async function init() {
@@ -21,12 +11,6 @@ async function init() {
     console.error("Failed to initialize:", error);
   }
 }
-
-document.getElementById("modal-close").addEventListener("click", () => {
-  document.getElementById("modal").classList.add("hidden");
-});
-
-document.getElementById("modal-close").addEventListener("click", () => modal.classList.add("hidden"));
 
 // Start the application
 init();
